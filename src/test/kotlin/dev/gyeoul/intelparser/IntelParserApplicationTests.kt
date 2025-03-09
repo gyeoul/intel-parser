5b0d154a7b2860508f98d1d91935dcef11d7f9da
Gyeoul <clsmay.1st@gmail.com>
2025-03-09 22:54:28 +0900

    [미션1] 깃허브 리포지토리에 프로젝트 올리기

---
+++ src/test/kotlin/dev/gyeoul/intelparser/IntelParserApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.gyeoul.intelparser
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IntelParserApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
